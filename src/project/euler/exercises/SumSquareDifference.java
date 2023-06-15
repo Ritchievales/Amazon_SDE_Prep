2681aacb330e20ab31496fe4fb40d43065c3216b
Ritchievales <ritchievales@gmail.com>
2023-06-15 12:50:28 -0600

    Add SumSquareDifference problem possible solution

---
+++ src/project/euler/exercises/SumSquareDifference.java
@@ -0,0 +1,31 @@
+package project.euler.exercises;
+
+public class SumSquareDifference {
+
+    public int getSumOfSquares(int n){
+        System.out.println(n);
+
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            sum+=(i*i);
+        }
+        System.out.println(sum);
+        return  sum;
+    }
+
+    public int getSquareOfSum(int n){
+        System.out.println(n);
+
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum*sum);
+        return sum*sum;
+    }
+
+    public int getSumSquareDifference(int n){
+        System.out.println(n);
+        return getSumOfSquares(n)-getSquareOfSum(n);
+    }
+}
