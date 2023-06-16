680f85d14f01611e5cc037e9e18891d38488c705
Ritchievales <ritchievales@gmail.com>
2023-06-15 18:23:29 -0600

    Progress of the last hour

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+import project.euler.exercises.HighlyDivisibleTriangularNumber;
+import project.euler.exercises.LargestProductInAGrid;
+import project.euler.exercises.SumationOfPrimes;
+
+public class Main {
+    public static void main(String[] args){
+        HighlyDivisibleTriangularNumber h = new HighlyDivisibleTriangularNumber();
+        h.getTriangleNumberWith500Divisors();
+    }
 }
